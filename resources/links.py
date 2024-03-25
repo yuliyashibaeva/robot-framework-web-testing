e9c8b85d3ebb818b3f07c188bf69e7e82707c75b
yuliya.shibaeva <yuliya.shibaeva.ys@gmail.com>
2024-03-25 22:14:22 +0200

    added links

---
+++ resources/links.py
@@ -0,0 +1,4 @@
+MAIN_PAGE_LINK = "https://magento.softwaretestingboard.com/"
+JUNO_JACKET_PRODUCT_PAGE_LINK = "https://magento.softwaretestingboard.com/juno-jacket.html"
+NADIA_ELEMENTS_SHELL_PRODUCT_PAGE_LINK = "https://magento.softwaretestingboard.com/nadia-elements-shell.html"
+CART_PAGE_LINK = "https://magento.softwaretestingboard.com/checkout/cart/"
